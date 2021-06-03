05c5daec774594ff3c503feb2a11e38aebb1fb75
dwu1200 <daniel.wu.1@stonybrook.edu>
2021-06-03 01:20:35 -0400

    organized

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,42 +1,31 @@
-//#include <iostream>
-//using namespace std;
-//
-//// fast integer in
-//void fastscan(int& ret) {
-//    ret = 0;
-//    char r;
-//    bool start = false, neg = false;
-//    while (true) {
-//        r = getchar();
-//        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && !start) {
-//            continue;
-//        }
-//        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && start) {
-//            break;
-//        }
-//        if (start)ret *= 10;
-//        start = true;
-//        if (r == '-')neg = true;
-//        else ret += r - '0';
-//    }
-//    if (neg) ret *= -1;
-//}
-//
-//    // Keep on extracting characters if they are integers
-//    // i.e ASCII Value lies from '0'(48) to '9' (57)
-//    for (; (c > 47 && c < 58); c = getchar())
-//        number = number * 10 + c - 48;
-//
-//    // if scanned input has a negative sign, negate the
-//    // value of the input number
-//    if (negative)
-//        number *= -1;
-//}
-//
-//int main()
-//{
-//    // use "\n" instead of cout << endl
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+// fast integer in
+void fastscan(int& ret) {
+    ret = 0;
+    char r;
+    bool start = false, neg = false;
+    while (true) {
+        r = getchar();
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && !start) {
+            continue;
+        }
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && start) {
+            break;
+        }
+        if (start)ret *= 10;
+        start = true;
+        if (r == '-')neg = true;
+        else ret += r - '0';
+    }
+    if (neg) ret *= -1;
+}
+
+int main()
+{
+    // use "\n" instead of cout << endl
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
---
+++ finished problems/unionfind.cpp
@@ -0,0 +1,91 @@
+//https://open.kattis.com/problems/unionfind
+#include <iostream>
+
+using namespace std;
+
+class union_find
+{
+    int* id, cnt, * sz;
+public:
+    // Create an empty union find data structure with N isolated sets.
+    union_find(int N) {
+        cnt = N; id = new int[N]; sz = new int[N];
+        for (int i = 0; i < N; i++)  id[i] = i, sz[i] = 1;
+    }
+    ~union_find() { delete[] id; delete[] sz; }
+
+    // Return the id of component corresponding to object p.
+    int find(int p) {
+        int root = p;
+        while (root != id[root])    root = id[root];
+        while (p != root) { int newp = id[p]; id[p] = root; p = newp; }
+        return root;
+    }
+    // Replace sets containing x and y with their union.
+    void merge(int x, int y) {
+        int i = find(x); int j = find(y); if (i == j) return;
+        // make smaller root point to larger one
+        if (sz[i] < sz[j]) { id[i] = j, sz[j] += sz[i]; }
+        else { id[j] = i, sz[i] += sz[j]; }
+        cnt--;
+    }
+    // Are objects x and y in the same set?
+    bool connected(int x, int y) { return find(x) == find(y); }
+    // Return the number of disjoint sets.
+    int count() { return cnt; }
+};
+
+void readint(int& ret) {
+    ret = 0;
+    char r;
+    bool start = false, neg = false;
+    while (true) {
+        r = getchar();
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && !start) {
+            continue;
+        }
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && start) {
+            break;
+        }
+        if (start)ret *= 10;
+        start = true;
+        if (r == '-')neg = true;
+        else ret += r - '0';
+    }
+    if (neg) ret *= -1;
+}
+
+int main()
+{
+    int n, q;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    readint(n);
+    readint(q);
+
+    union_find uf = union_find(n + 1);
+    char qr;
+    int a, b;
+    for (int i = 0; i < q; i++)
+    {
+        qr = getchar();
+        readint(a);
+        readint(b);
+        if (qr == '?')
+        {
+            if (uf.connected(a, b))
+            {
+                cout << "yes" << endl;
+            }
+            else
+            {
+                cout << "no" << endl;
+            }
+        }
+        else
+        {
+            uf.merge(a, b);
+        }
+    }
+    return 0;
+}
