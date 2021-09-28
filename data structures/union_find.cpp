dcdf51ffd289de0085e3a081afa2dc817f9b178f
dwu1200 <daniel.wu.1@stonybrook.edu>
2021-09-28 13:52:19 -0400

    aaaa

--- data structures/union_find.cpp
+++ data structures/union_find.cpp
@@ -30,3 +30,42 @@ public:
     int count() { return cnt; }
 };
 
+class UF:
+    """An implementation of union find data structure.
+    It uses weighted quick union by rank with path compression.
+    """
+    def __init__(self, N):
+        self._id = list(range(N))
+        self._count = N
+        self._rank = [1] * N
+    def find(self, p):
+        """Find the set identifier for the item p."""
+        id = self._id
+        root = p
+        while root != id[root]:
+            root = id[root]
+        while p != root:
+            newp = id[p]
+            id[p] = root
+            p = newp
+        return p
+    def count(self):
+        """Return the number of items."""
+        return self._count
+    def connected(self, p, q):
+        """Check if the items p and q are on the same set or not."""
+        return self.find(p) == self.find(q)
+    def union(self, p, q):
+        id = self._id
+        rank = self._rank
+        i = self.find(p)
+        j = self.find(q)
+        if i == j:
+            return
+        if rank[i] < rank[j]:
+            id[i] = j
+            rank[j] += rank[i]
+        else:
+            id[j] = i
+            rank[i] += rank[j]
+        self._count -= 1
