71cc300c56a7180e1fd8622ccc2a5b7e50fb821d
dwu1200 <daniel.wu.1@stonybrook.edu>
2021-09-28 17:21:11 -0400

    Update TEMPLATE.cpp

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,31 +1,31 @@
-//#include <iostream>
-//using namespace std;
-//
-//// fast integer in
-//void fastscan(int& ret) {
-//    ret = 0;
-//    char r;
-//    bool start = false, neg = false;
-//    while (true) {
-//        r = getchar();
-//        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && !start) {
-//            continue;
-//        }
-//        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && start) {
-//            break;
-//        }
-//        if (start)ret *= 10;
-//        start = true;
-//        if (r == '-')neg = true;
-//        else ret += r - '0';
-//    }
-//    if (neg) ret *= -1;
-//}
-//
-//int main()
-//{
-//    // use "\n" instead of cout << endl
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+// fast integer in
+void fastscan(int& ret) {
+    ret = 0;
+    char r;
+    bool start = false, neg = false;
+    while (true) {
+        r = getchar();
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && !start) {
+            continue;
+        }
+        if ((r - '0' < 0 || r - '0' > 9) && r != '-' && start) {
+            break;
+        }
+        if (start)ret *= 10;
+        start = true;
+        if (r == '-')neg = true;
+        else ret += r - '0';
+    }
+    if (neg) ret *= -1;
+}
+
+int main()
+{
+    // use "\n" instead of cout << endl
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
