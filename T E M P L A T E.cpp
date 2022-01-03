6feba3b3f999c5e8fcf2189e3e10f92b061c4eb8
dwu1200 <daniel.wu.1@stonybrook.edu>
2022-01-02 22:29:37 -0500

    finished 1594-E2 from last year
    solved a 2300!!

--- T E M P L A T E.cpp
+++ T E M P L A T E.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 typedef long long ll;
 typedef pair<int, int> p;
 
 ll inf32 = 1e18;
+ll mod = 1e9 + 7; 
+
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
  
 template <typename T> void output_vector(vector<T> V) {
     for (auto v : V) cout << v << ' ';
@@ -12,27 +15,60 @@ template <typename T> void output_vector(vector<T> V) {
 }
 
 void solve() {
-    int n, x; cin >> n;
-    vector<int> A(n); for (int &a : A) cin >> a;
-    cin >> x;
-    for (int &a : A) a -= x;
-    int ret = 0; ll s = inf32;
-    for (int &a : A) {
-        s += a;
-        if (s < 0) s = inf32;
-        else {
-            ret++;
-            s = min(s, (ll) a);
+    ll k, n; cin >> k >> n;
+    map<ll, ll, greater<ll>> colors;
+    map<ll, vector<ll>> marked;
+    for (int i = 0; i < n; i++) {
+        ll v; string s; cin >> v >> s;
+        if (s == "white" || s == "yellow") colors[v] = 0;
+        else if (s == "green" || s == "blue") colors[v] = 1;
+        else colors[v] = 2;
+        while (v >= 1) {
+            marked[v] = vector<ll>(6);
+            v /= 2;
         }
     }
-    cout << ret << '\n';
+    for (const auto &[vertex, c] : colors) {
+        ll v = vertex;
+        while (v >= 1) {
+            vector<ll> temp(6, 1);
+            if (marked.count(v * 2)) {
+                temp[0] *= (marked[v * 2][2] + marked[v * 2][3] + marked[v * 2][4] + marked[v * 2][5]) % mod; temp[0] %= mod;
+                temp[1] = temp[0];
+                temp[2] *= (marked[v * 2][4] + marked[v * 2][5] + marked[v * 2][0] + marked[v * 2][1]) % mod; temp[2] %= mod;
+                temp[3] = temp[2];
+                temp[4] *= (marked[v * 2][0] + marked[v * 2][1] + marked[v * 2][2] + marked[v * 2][3]) % mod; temp[4] %= mod;
+                temp[5] = temp[4];
+            }
+            if (marked.count(v * 2 + 1)) {
+                temp[0] *= (marked[v * 2 + 1][2] + marked[v * 2 + 1][3] + marked[v * 2 + 1][4] + marked[v * 2 + 1][5]) % mod; temp[0] %= mod;
+                temp[1] = temp[0];
+                temp[2] *= (marked[v * 2 + 1][4] + marked[v * 2 + 1][5] + marked[v * 2 + 1][0] + marked[v * 2 + 1][1]) % mod; temp[2] %= mod;
+                temp[3] = temp[2];
+                temp[4] *= (marked[v * 2 + 1][0] + marked[v * 2 + 1][1] + marked[v * 2 + 1][2] + marked[v * 2 + 1][3]) % mod; temp[4] %= mod;
+                temp[5] = temp[4];
+            }
+            if (colors.count(v)) {
+                for (int i = 0; i < 6; i++) {
+                    if (i != colors[v] * 2) {
+                        temp[i] = 0;
+                    }
+                }
+            }
+            marked[v] = temp;
+            v /= 2;
+        }
+    }
+    ll untotal = powmod(4, (1LL << k) - 1 - marked.size());
+    ll total = accumulate(marked[1].begin(), marked[1].end(), 0LL) % mod;
+    cout << (untotal * total) % mod;
 }
- 
+
 int main() 
 {
     // use "\n" instead of cout << endl
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); solve(); return 0;
     int t; cin >> t; while (t--) {
         solve();
     }
--- codeforces/1594-E2.cpp
+++ codeforces/1594-E2.cpp
@@ -1,51 +1,76 @@
-//unfinished
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-long long t, n, k, ret, x, y, z, N = 1000000007;
+
+typedef long long ll;
 typedef pair<int, int> p;
-typedef map<int, int> mp;
-string s;
-long long exponentiation(long long base,
-                        long long exp, long long N)
-{
-    if (exp == 0)
-        return 1;
-    if (exp == 1)
-        return base % N;
-    long long t = exponentiation(base, exp / 2, N);
-    t = (t * t) % N;
-    if (exp % 2 == 0)
-        return t;
-    else
-        return (base * t) % N;
+
+ll inf32 = 1e18;
+ll mod = 1e9 + 7; 
+
+ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ 
+template <typename T> void output_vector(vector<T> V) {
+    for (auto v : V) cout << v << ' ';
+    cout << '\n';
 }
 
-mp predefined = mp();
-set<int> marked;
-int ret = 0;
-
-void solve(long long dp[3], long long curr) {
-
-}
-
-int main() {
-    cin >> k;
-    cin >> n;
-    while (n--) {
-        cin >> t;
-        cin >> s;
-        if (s == "yellow" || s == "white") x = 0;
-        if (s == "green" || s == "blue") x = 1;
-        if (s == "red" || s == "orange") x = 2;
-        predefined[t] = x;
-        while (t > 0 && !marked.count(t)) {
-            marked.insert(t);
-            t /= 2;
+void solve() {
+    ll k, n; cin >> k >> n;
+    map<ll, ll, greater<ll>> colors;
+    map<ll, vector<ll>> marked;
+    for (int i = 0; i < n; i++) {
+        ll v; string s; cin >> v >> s;
+        if (s == "white" || s == "yellow") colors[v] = 0;
+        else if (s == "green" || s == "blue") colors[v] = 1;
+        else colors[v] = 2;
+        while (v >= 1) {
+            marked[v] = vector<ll>(6);
+            v /= 2;
         }
+    }
+    for (const auto &[vertex, c] : colors) {
+        ll v = vertex;
+        while (v >= 1) {
+            vector<ll> temp(6, 1);
+            if (marked.count(v * 2)) {
+                temp[0] *= (marked[v * 2][2] + marked[v * 2][3] + marked[v * 2][4] + marked[v * 2][5]) % mod; temp[0] %= mod;
+                temp[1] = temp[0];
+                temp[2] *= (marked[v * 2][4] + marked[v * 2][5] + marked[v * 2][0] + marked[v * 2][1]) % mod; temp[2] %= mod;
+                temp[3] = temp[2];
+                temp[4] *= (marked[v * 2][0] + marked[v * 2][1] + marked[v * 2][2] + marked[v * 2][3]) % mod; temp[4] %= mod;
+                temp[5] = temp[4];
+            }
+            if (marked.count(v * 2 + 1)) {
+                temp[0] *= (marked[v * 2 + 1][2] + marked[v * 2 + 1][3] + marked[v * 2 + 1][4] + marked[v * 2 + 1][5]) % mod; temp[0] %= mod;
+                temp[1] = temp[0];
+                temp[2] *= (marked[v * 2 + 1][4] + marked[v * 2 + 1][5] + marked[v * 2 + 1][0] + marked[v * 2 + 1][1]) % mod; temp[2] %= mod;
+                temp[3] = temp[2];
+                temp[4] *= (marked[v * 2 + 1][0] + marked[v * 2 + 1][1] + marked[v * 2 + 1][2] + marked[v * 2 + 1][3]) % mod; temp[4] %= mod;
+                temp[5] = temp[4];
+            }
+            if (colors.count(v)) {
+                for (int i = 0; i < 6; i++) {
+                    if (i != colors[v] * 2) {
+                        temp[i] = 0;
+                    }
+                }
+            }
+            marked[v] = temp;
+            v /= 2;
+        }
+    }
+    ll untotal = powmod(4, (1LL << k) - 1 - marked.size());
+    ll total = accumulate(marked[1].begin(), marked[1].end(), 0LL) % mod;
+    cout << (untotal * total) % mod;
+}
+
+int main() 
+{
+    // use "\n" instead of cout << endl
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); solve(); return 0;
+    int t; cin >> t; while (t--) {
+        solve();
     }
     return 0;
 }
