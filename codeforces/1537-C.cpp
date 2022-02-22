6ec6e36fc407b0e47dee18c37d03b7c904d7a913
dwu1200 <daniel.wu.1@stonybrook.edu>
2022-02-22 17:57:17 -0500

    moving to linux

---
+++ codeforces/1537-C.cpp
@@ -0,0 +1,59 @@
+#define interactive 0
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define ROF(i,a,b) for (int i = (b) - 1; i >= (a); --i)
+#define REP(a) FOR(_,0,a)
+#define EACH(a,x) (auto& a: x)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<ll, ll> pl;
+
+ll mod = 1e9 + 7; //998244353;
+int BIG32 = 1e9 + 5;
+
+ll powmod(ll a,ll b) {ll res = 1; a %= mod; assert(b >= 0); for(; b; b >>= 1){if (b & 1) res = res * a % mod; a = a * a % mod;} return res;}
+
+template <typename T> void output_vector(vector<T> &V, bool spaces=true) {for (auto v : V) {if (spaces) cout << v << ' '; else cout << v;} if (interactive) cout << endl; else cout << '\n';}
+template <typename T> T max(vector<T> &V) {return *max_element(V.begin(), V.end());}
+template <typename T> T min(vector<T> &V) {return *min_element(V.begin(), V.end());}
+template <typename T> T sum(vector<T> &V) {return accumulate(V.begin(), V.end(), 0);}
+template <typename T> int sgn(T val) {return (T(0) < val) - (val < T(0));}
+template <typename T> T bisect_left(vector<T> &V, T x) {return lower_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+template <typename T> T bisect_right(vector<T> &V, T x) {return upper_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+
+void solve() {
+    int n; cin >> n;
+    vector<int> A(n); for EACH(a, A) cin >> a;
+    sort(A.begin(), A.end());
+    if (A.size() == 2) {
+        output_vector(A);
+        return;
+    }
+    int mindiff = BIG32, mindex = -1;
+    FOR(i, 1, n) {
+        if (A[i] - A[i - 1] < mindiff) {
+            mindiff = A[i] - A[i - 1];
+            mindex = i;
+        }
+    }
+    FOR(i, mindex, n) cout << A[i] << ' ';
+    FOR(i, 0, mindex) cout << A[i] << ' ';
+    cout << '\n';
+}
+
+int main() 
+{
+    cout.tie(nullptr); cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    if (interactive) solve();
+    else {
+        int t; cin >> t; REP(t) {
+            solve();
+        }
+    }
+    return 0;
+}
