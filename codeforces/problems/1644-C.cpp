bddcea35fee0066b169f8de505d4593204234695
Daniel Wu <daniel.wu.1@stonybrook.edu>
2022-02-22 16:57:46 -0500

    codeforces edu 123

---
+++ codeforces/problems/1644-C.cpp
@@ -0,0 +1,63 @@
+#define interactive 0
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define ROF(i,a,b) for (int i = (b) - 1; i >= (a); --i)
+#define REP(a) FOR(_,0,a)
+#define EACH(a,x) (auto& a: x)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<ll, ll> pl;
+
+ll mod = 1e9 + 7; //998244353;
+int BIG32 = 1e9 + 5;
+
+ll powmod(ll a,ll b) {ll res = 1; a %= mod; assert(b >= 0); for(; b; b >>= 1){if (b & 1) res = res * a % mod; a = a * a % mod;} return res;}
+
+template <typename T> void output_vector(vector<T> &V, bool spaces=true) {for (auto v : V) {if (spaces) cout << v << ' '; else cout << v;} if (interactive) cout << endl; else cout << '\n';}
+template <typename T> T max(vector<T> &V) {return *max_element(V.begin(), V.end());}
+template <typename T> T min(vector<T> &V) {return *min_element(V.begin(), V.end());}
+template <typename T> T sum(vector<T> &V) {return accumulate(V.begin(), V.end(), 0);}
+template <typename T> int sgn(T val) {return (T(0) < val) - (val < T(0));}
+template <typename T> T bisect_left(vector<T> &V, T x) {return lower_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+template <typename T> T bisect_right(vector<T> &V, T x) {return upper_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+
+void solve() {
+    int n, x; cin >> n >> x;
+    vector<int> A(n); for EACH(a, A) cin >> a;
+    vector<vector<int>> dp(n, vector<int>(n + 1, 0));
+    dp[0][0] = A[0];
+    FOR(i, 1, n) dp[i][0] = max(A[i], A[i] + dp[i - 1][0]);
+    dp[0][1] = A[0] + x;
+    FOR(i, 1, n) {
+        FOR(j, 1, i + 2) {
+            dp[i][j] = max(A[i] + x, dp[i - 1][j - 1] + A[i] + x);
+        }
+    }
+    int prev = 0;
+    FOR(j, 0, n + 1) {
+        int m = 0;
+        FOR(i, 0, n) {
+            m = max(m, dp[i][j]);
+        }
+        cout << max(m, prev) << ' ';
+        prev = max(m, prev);
+    }
+    cout << '\n';
+}
+
+int main() 
+{
+    cout.tie(nullptr); cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    if (interactive) solve();
+    else {
+        int t; cin >> t; REP(t) {
+            solve();
+        }
+    }
+    return 0;
+}
