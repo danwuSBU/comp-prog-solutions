9f7142387530fd3d27b923b4db046bb2e039bfa3
dwu1200 <daniel.wu.1@stonybrook.edu>
2022-06-07 22:23:11 -0400

    codeforces 796

---
+++ codeforces/problems/1688-D.cpp
@@ -0,0 +1,62 @@
+#define interactive 0
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define ROF(i,a,b) for (int i = (b) - 1; i >= (a); --i)
+#define REP(a) FOR(_,0,a)
+#define EACH(a,x) (auto& a: x)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<ll, ll> pl;
+
+ll mod = /*1e9 + 7;*/ 998244353;
+int BIG32 = 1e9 + 5;
+
+ll powmod(ll a,ll b) {ll res = 1; a %= mod; assert(b >= 0); for(; b; b >>= 1){if (b & 1) res = res * a % mod; a = a * a % mod;} return res;}
+
+template <typename T> void output_vector(vector<T> &V, bool spaces=true) {for (auto v : V) {if (spaces) cout << v << ' '; else cout << v;} if (interactive) cout << endl; else cout << '\n';}
+template <typename T> T max(vector<T> &V) {return *max_element(V.begin(), V.end());}
+template <typename T> T min(vector<T> &V) {return *min_element(V.begin(), V.end());}
+template <typename T> T sum(vector<T> &V) {return accumulate(V.begin(), V.end(), T(0));}
+template <typename T> int bisect_left(vector<T> &V, T x) {return lower_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+template <typename T> int bisect_right(vector<T> &V, T x) {return upper_bound(V.begin(), V.end(), x) - V.begin();} // must be sorted
+
+// preprocess //
+void prepare() {
+
+}
+// preprocess //
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    vector<ll> A(n); for EACH(a, A) cin >> a;
+    if (n >= k) {
+        ll s = 0;
+        FOR(i, 0, k) s += A[i];
+        ll b = s;
+        FOR(i, k, n) {
+            s += A[i];
+            s -= A[i - k];
+            b = max(b, s);
+        }
+        cout << b + k * (k - 1) / 2 << '\n';
+    }
+    else cout << sum(A) + n * (k - 1) - (n) * (n - 1) / 2 << '\n';
+}
+
+int main()
+{
+    cout.tie(nullptr); cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    prepare();
+    if (interactive) solve();
+    else {
+        int t; cin >> t; REP(t) {
+            solve();
+        }
+    }
+    return 0;
+}
